2bb6232d0269d262522c6ee79017e3c131d461d1
Jorgelizarraga94 <jorgelizarraga1994@gmail.com>
2026-07-08 19:59:10 -0300

    implemento y chequeo el funcionamiento del Api Gateway

--- cart-service/src/main/java/com/carrito/carrito/CarritoApplication.java
+++ cart-service/src/main/java/com/carrito/carrito/CarritoApplication.java
@@ -2,11 +2,13 @@ package com.carrito.carrito;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class CarritoApplication {
 
 	public static void main(String[] args) {
--- sales-service/src/main/java/com/ventas/ventas/VentasApplication.java
+++ sales-service/src/main/java/com/ventas/ventas/VentasApplication.java
@@ -2,10 +2,12 @@ package com.ventas.ventas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class VentasApplication {
 
 	public static void main(String[] args) {
